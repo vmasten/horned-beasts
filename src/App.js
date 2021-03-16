aba987b9e79a911d365bfe5a2f656904af2b4442
Vince Masten <vmasten@me.com>
2021-03-15 18:27:42 -0700

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Header from './header';
 import Footer from './footer';
+import Main from './main';
+
+import pepper from './img/pepper.JPG';
+import george from './img/george.jpg';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Main title='Pepper' source={pepper} alt='dog holding a turkey feather' description='Very pleased with herself after finding this feather'/>
+      <Main title='George' source={george} alt='cat in blanket with paws crossed' description='Looking regal and contented, as a cat should' />
       <Footer />
     </div>
   );
