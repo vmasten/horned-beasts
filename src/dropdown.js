47893f53c44b69122ecf498c8756854cca7b219a
Vince Masten <vmasten@me.com>
2021-03-18 21:36:51 -0700

    stopped it from blowing up, but dropdown is still nonfunctional

--- src/dropdown.js
+++ src/dropdown.js
@@ -16,7 +16,7 @@ class DropdownInfo extends React.Component {
     return (
   <Dropdown>
   <DropdownButton id="dropdown-item-button" title="Horns">
-    <Dropdown.Item as="button" value={1} onSelect={this.props.setHorns(value)}>1</Dropdown.Item>
+    <Dropdown.Item as="button" value={1} onSelect={this.props.setHorns}>1</Dropdown.Item>
     <Dropdown.Item as="button">2</Dropdown.Item>
     <Dropdown.Item as="button">3</Dropdown.Item>
     <Dropdown.Item as="button">many</Dropdown.Item>
